7e68447b89caa789b91bb61fe479599e7eb8200d
randikaweerasinghe <wms.randika03@gmail.com>
2025-02-19 09:58:29 +0530

    Added new file and updated second.c file

---
+++ first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+
+        printf("Welcome to the Fundamental Programming Session!\n");
+        printf("Randika Weeerasinghe");
+        printf("2/19/2025")
+
+        return 0;
+}
